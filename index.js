7d21bb9e7b2ec9147fc04efd35e67625aea2ef7f
Fábio Ferreira <fabiomataferreira@gmail.com>
2020-10-31 12:31:12 -0400

    Remove e edita registros

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+let request;
+if (!window.indexedDB) {
+  console.log("Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available.");
+} else {
+  request = window.indexedDB.open("ControleFinanceiro", 3)
+}
+
 const formatter = new Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'});
 
 function extraiNumeros(string) {
@@ -10,14 +17,16 @@ function somaReducer(a, b) {
 }
 
 function AppViewModel() {
-  this.descricao = ko.observable('');
-  this.valor = ko.observable('');
-  this.tipo = ko.observable('despesa');
+  const self = this;
 
-  this.despesas = ko.observableArray();
-  this.receitas = ko.observableArray();
+  self.descricao = ko.observable('');
+  self.valor = ko.observable('');
+  self.tipo = ko.observable('despesa');
 
-  this.handleValor = function () {
+  self.despesas = ko.observableArray();
+  self.receitas = ko.observableArray();
+
+  self.handleValor = function () {
     const valor = this.valor();
     if(!valor) return;
     const numero = extraiNumeros(valor);
@@ -25,33 +34,53 @@ function AppViewModel() {
 
     this.valor(dinheiro)
   }
-  this.salva = function () {
+  self.salva = function () {
     const descricao = this.descricao();
     const valor = this.valor();
+    const tipo = this.tipo();
     if(!descricao || !valor || parseFloat(extraiNumeros(valor)) === 0) {
       alert('Verifique os campos obrigatórios.');
       return;
     }
-    if(this.tipo() === 'despesa') {
-      this.despesas.push({descricao, valor});
+    if(tipo === 'despesa') {
+      this.despesas.push({descricao, valor, tipo});
     } else {
-      this.receitas.push({descricao, valor});
+      this.receitas.push({descricao, valor, tipo});
     }
     this.descricao('');
     this.valor('');
     alert('Salvo com sucesso!');
   }
 
-  this.saldo = ko.computed(() => {
-    const despesas = this.despesas.slice();
-    const receitas = this.receitas.slice();
+  self.saldo = ko.computed(() => {
+    const despesas = self.despesas.slice();
+    const receitas = self.receitas.slice();
     const totalDespesas = despesas.length ? despesas.map(d => extraiNumeros(d.valor)).reduce(somaReducer) : 0;
     const totalReceitas = receitas.length ?  receitas.map(d => extraiNumeros(d.valor)).reduce(somaReducer) : 0;
 
-    console.log(totalDespesas, totalReceitas)
-
     return formatter.format(totalReceitas - totalDespesas);
   })
+
+  self.remover = function(registro) {
+    if(!confirm("Tem certeza que deseja excluir esse registro?")) return;
+    if(registro.tipo === 'despesa') {
+      self.despesas.remove(registro);
+    } else {
+      self.receitas.remove(registro);
+    }
+  }
+
+  self.editar = function(registro) {
+    if(!confirm("Tem certeza que deseja editar esse registro?")) return;
+    if(registro.tipo === 'despesa') {
+      self.despesas.remove(registro);
+    } else {
+      self.receitas.remove(registro);
+    }
+    self.descricao(registro.descricao);
+    self.valor(registro.valor);
+    self.tipo(registro.tipo);
+  }
 }
 
 ko.applyBindings(new AppViewModel());
