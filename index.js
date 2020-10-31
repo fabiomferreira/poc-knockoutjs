d3d045945fb607e7b28e34accd9c94a16acc6378
Fábio Ferreira <fabiomataferreira@gmail.com>
2020-10-31 11:23:38 -0400

    Insere validação

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const formatter = new Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'});
+function extraiNumeros(string) {
+    const numeros = string.match(/\d+/g); 
+    return numeros ? numeros.join('') / 100 : 0;
+} 
 function AppViewModel() {
   this.descricao = ko.observable('');
   this.valor = ko.observable('');
@@ -7,15 +11,22 @@ function AppViewModel() {
   this.handleValor = function () {
     const valor = this.valor();
     if(!valor) return;
-    const numeros = valor.match(/\d+/g); 
-    const numero = numeros ? numeros.join('') / 100 : 0;
+    const numero = extraiNumeros(valor);
     const dinheiro = formatter.format(numero);
 
     this.valor(dinheiro)
   }
   this.salva = function () {
+    const descricao = this.descricao();
+    const valor = this.valor();
+    if(!descricao || !valor || parseFloat(extraiNumeros(valor)) === 0) {
+      alert('Verifique os campos obrigatórios.');
+      return;
+    }
     this.despesas.push({descricao: this.descricao(), valor: this.valor()});
-    console.log(this.descricao(), this.valor());
+    this.descricao('');
+    this.valor('');
+    alert('Salvo com sucesso!');
   }
 }
 
