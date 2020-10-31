b2c1c340a64d3ee0f84dd6838297ce1a97522323
Fábio Ferreira <fabiomataferreira@gmail.com>
2020-10-31 11:12:22 -0400

    Salva despesas

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+const formatter = new Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'});
 function AppViewModel() {
   this.descricao = ko.observable('');
   this.valor = ko.observable('');
+
+  this.despesas = ko.observableArray()
+  this.handleValor = function () {
+    const valor = this.valor();
+    if(!valor) return;
+    const numeros = valor.match(/\d+/g); 
+    const numero = numeros ? numeros.join('') / 100 : 0;
+    const dinheiro = formatter.format(numero);
+
+    this.valor(dinheiro)
+  }
   this.salva = function () {
-    console.log(this.descricao());
+    this.despesas.push({descricao: this.descricao(), valor: this.valor()});
+    console.log(this.descricao(), this.valor());
   }
 }
 
